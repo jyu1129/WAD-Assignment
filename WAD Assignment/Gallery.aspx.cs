bf0c850defb371f1b957dd0ff7ba6ea7a1222d64
Justin Yu <lipohshun1@gmail.com>
2021-02-24 03:37:47 +0800

    aw1

--- WAD Assignment/Gallery.aspx.cs
+++ WAD Assignment/Gallery.aspx.cs
@@ -13,5 +13,10 @@ namespace WAD_Assignment
         {
 
         }
+
+        protected void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- WAD Assignment/Gallery.aspx.designer.cs
+++ WAD Assignment/Gallery.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace WAD_Assignment
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// ListView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView ListView1;
+
+        /// <summary>
         /// SqlDataSource1 control.
         /// </summary>
         /// <remarks>
