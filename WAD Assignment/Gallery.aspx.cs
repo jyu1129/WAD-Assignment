48f6f18e412c2d3f236dbacb96f3643ce9a4156b
Justin Yu <lipohshun1@gmail.com>
2021-02-27 07:09:23 +0800

    sdsds

--- WAD Assignment/Gallery.aspx.cs
+++ WAD Assignment/Gallery.aspx.cs
@@ -17,10 +17,15 @@ namespace WAD_Assignment
         protected void Page_Load(object sender, EventArgs e)
         {
             lblGalleryTotal.Text = DataList1.Items.Count.ToString();
-
             string userId = Session["userID"].ToString();
-            //string strSelect = "SELECT * FROM Arts WHERE ArtId = " + artId;
-            //SqlCommand cmdSelect = new SqlCommand(strSelect, con);
+            con = new SqlConnection(strCon);
+            con.Open();
+            string strSelect = "SELECT COUNT(OrderDetails.OrderDetailId) AS Expr1 FROM OrderDetails INNER JOIN" +
+                        " Orders ON OrderDetails.OrderId = Orders.OrderId INNER JOIN Arts ON OrderDetails.ArtId = Arts.ArtId" +
+                        " WHERE Arts.ArtistId = " + userId;
+            SqlCommand cmdSelect = new SqlCommand(strSelect, con);
+            lblTotalArtSold.Text = cmdSelect.ExecuteScalar().ToString();
+            con.Close();
         }
 
         protected void ListView1_SelectedIndexChanged(object sender, EventArgs e)
--- WAD Assignment/SellArt.aspx.designer.cs
+++ WAD Assignment/SellArt.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace WAD_Assignment
         protected global::System.Web.UI.WebControls.Button btnEdit;
 
         /// <summary>
+        /// spinner control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl spinner;
+
+        /// <summary>
         /// UpdatePanel1 control.
         /// </summary>
         /// <remarks>
@@ -67,6 +76,15 @@ namespace WAD_Assignment
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Image uploadedImg;
+
+        /// <summary>
+        /// txtArtUrl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtArtUrl;
 
         /// <summary>
         /// validImage control.
