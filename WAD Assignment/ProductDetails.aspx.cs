6cf13a186cfab3fd5b17b3970c29e7957afc5589
winnie2480 <winnie2480@hotmail.com>
2021-02-27 06:04:14 +0800

    update cart qty

--- WAD Assignment/ProductDetails.aspx.cs
+++ WAD Assignment/ProductDetails.aspx.cs
@@ -41,24 +41,80 @@ namespace WAD_Assignment
 
                 else if (e.CommandName == "AddToCart")
                 {
-
-                    //create database connection
+                    bool itemExisted = false;
                     con = new SqlConnection(strCon);
-                    //open database
                     con.Open();
-                    //To Get Art Id from the of the item from datalist
+                    string strSelect = "SELECT * FROM Cart WHERE CustomerId=" + Session["userID"];
+                    SqlCommand cmdInsert4 = new SqlCommand(strSelect, con);
+                    SqlDataReader drArtId = cmdInsert4.ExecuteReader();
                     Label ArtId = e.Item.FindControl("ArtIdLabel") as Label;
-                    int Quantity = 1;
-                    //INSERT database record
-                    string strInsert = "INSERT INTO Cart(CustomerId, ArtId, Quantity) VALUES(" + Session["UserID"] + "," + ArtId.Text.ToString() + "," + Quantity + ")";
-                    //create sqlcommand
-                    SqlCommand cmdInsert = new SqlCommand(strInsert, con);
-                    //temporary store record retrived by command object
-                    SqlDataReader dtrSelect = cmdInsert.ExecuteReader();
-                    //display success
-                    ClientScript.RegisterStartupScript(typeof(Page), "test", "<script>alert('Item successfully added to cart!');</script>");
-                    //close connection
+                    while (drArtId.Read())
+                    {
+                        if (drArtId["ArtId"].ToString().Equals(ArtId.Text.ToString()))
+                        {
+                            itemExisted = true;
+                        }
+                    }
                     con.Close();
+
+                    if (!itemExisted)
+                    {
+                        //create database connection
+                        con = new SqlConnection(strCon);
+                        //open database
+                        con.Open();
+                        //To Get Art Id from the of the item from datalist
+                        ArtId = e.Item.FindControl("ArtIdLabel") as Label;
+                        int Quantity = 1;
+                        //INSERT database record
+                        string strInsert = "INSERT INTO Cart(CustomerId, ArtId, Quantity) VALUES(" + Session["userID"] + "," + ArtId.Text.ToString() + "," + Quantity + ")";
+                        //create sqlcommand
+                        SqlCommand cmdInsert = new SqlCommand(strInsert, con);
+                        //temporary store record retrived by command object
+                        SqlDataReader dtrSelect = cmdInsert.ExecuteReader();
+                        //display success
+                        ClientScript.RegisterStartupScript(typeof(Page), "test", "<script>alert('Item successfully added to cart!');</script>");
+                        //close connection
+                        con.Close();
+                    }
+                    else
+                    {
+                        int Quantity = 0;
+
+                        con = new SqlConnection(strCon);
+                        con.Open();
+                        string strSelect2 = "SELECT * FROM Cart WHERE CustomerId=" + Session["userID"];
+                        SqlCommand cmdInsert5 = new SqlCommand(strSelect2, con);
+                        SqlDataReader drQty = cmdInsert5.ExecuteReader();
+                        while (drQty.Read())
+                        {
+                            if (int.Parse(drQty["Quantity"].ToString()) >= 1)
+                            {
+                                Quantity = int.Parse(drQty["Quantity"].ToString()) + 1;
+                            }
+                        }
+
+                        con.Close();
+
+                        //create database connection
+                        con = new SqlConnection(strCon);
+                        //open database
+                        con.Open();
+                        //To Get Art Id from the of the item from datalist
+                        ArtId = e.Item.FindControl("ArtIdLabel") as Label;
+
+                        //INSERT database record
+                        string strInsert = "UPDATE Cart SET Quantity = " + Quantity + " WHERE CustomerId = " + Session["userID"] + "AND ArtId = " + ArtId.Text.ToString();
+                        //create sqlcommand
+                        SqlCommand cmdInsert = new SqlCommand(strInsert, con);
+                        //temporary store record retrived by command object
+                        SqlDataReader dtrSelect = cmdInsert.ExecuteReader();
+                        //display success
+                        ClientScript.RegisterStartupScript(typeof(Page), "test", "<script>alert('Item +1 successfully added to cart!');</script>");
+                        //close connection
+                        con.Close();
+                    }
+
 
                 }
 
