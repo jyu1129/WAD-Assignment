5ee587f0a96b12ca3c83dd801ed5f6f899f2d8ce
Justin Yu <lipohshun1@gmail.com>
2021-02-28 06:42:38 +0800

    sa

--- WAD Assignment/ProductList.aspx.designer.cs
+++ WAD Assignment/ProductList.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace WAD_Assignment
         protected global::System.Web.UI.WebControls.DataList dtProduct;
 
         /// <summary>
+        /// dtProduct control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList dtProduct;
+
+        /// <summary>
         /// SqlDataSource1 control.
         /// </summary>
         /// <remarks>
