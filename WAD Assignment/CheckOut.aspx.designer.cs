333cc4bc56a38f60cb9677d26f509d26e2bc715e
ethan1289 <phangsingfong1289@gmail.com>
2021-04-05 02:59:33 +0800

    asd

--- WAD Assignment/CheckOut.aspx.designer.cs
+++ WAD Assignment/CheckOut.aspx.designer.cs
@@ -186,24 +186,6 @@ namespace WAD_Assignment
         protected global::System.Web.UI.WebControls.DropDownList ddlCountryList;
 
         /// <summary>
-        /// RFV5 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator RFV5;
-
-        /// <summary>
-        /// regex_txtState control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RegularExpressionValidator regex_txtState;
-
-        /// <summary>
         /// txtState control.
         /// </summary>
         /// <remarks>
