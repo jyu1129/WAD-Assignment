7b477da6ea16d03318d4be29bed1d96fbfc3120d
ethan1289 <phangsingfong1289@gmail.com>
2021-04-10 11:03:00 +0800

    asdasd

--- WAD Assignment/ProductDetails.aspx.cs
+++ WAD Assignment/ProductDetails.aspx.cs
@@ -14,33 +14,6 @@ namespace WAD_Assignment
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            //if (Session["user"] != null && (bool)Session["show"] == true)
-            //{
-            //    //wishlist heart
-            //    con = new SqlConnection(strCon);
-            //    con.Open();
-            //    string strSelect = "SELECT * FROM Wishlists WHERE CustomerId = " + Session["userID"];
-            //    SqlCommand cmdSelect = new SqlCommand(strSelect, con);
-            //    SqlDataReader drSelect = cmdSelect.ExecuteReader();
-            //    while (drSelect.Read())
-            //    {
-            //       if (drSelect["ArtId"].ToString().Equals(Request.QueryString["ArtId"].ToString()))
-            //       {
-
-            //          ((ImageButton)dtProductDetail.Items[0].FindControl("Button2")).ImageUrl = "https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/red_webpict35_1484337167-1.png";
-
-            //       }
-            //    }
-
-            //    drSelect.Close();
-            //    con.Close();
-            //    Session["show"] = false;
-            //    Response.Redirect("ProductDetails.aspx?ArtId=" + Request.QueryString["ArtId"].ToString());
-
-            //}
-
-
-
         }
 
         protected void dtProductDetail_ItemCommand(object source, DataListCommandEventArgs e)
@@ -83,22 +56,14 @@ namespace WAD_Assignment
 
                     if (!itemExisted)
                     {
-                        //create database connection
                         con = new SqlConnection(strCon);
-                        //open database
                         con.Open();
-                        //To Get Art Id from the of the item from datalist
                         ArtId = e.Item.FindControl("ArtIdLabel") as Label;
                         int Quantity = 1;
-                        //INSERT database record
                         string strInsert = "INSERT INTO Cart(CustomerId, ArtId, Quantity) VALUES(" + Session["userID"] + "," + ArtId.Text.ToString() + "," + Quantity + ")";
-                        //create sqlcommand
                         SqlCommand cmdInsert = new SqlCommand(strInsert, con);
-                        //temporary store record retrived by command object
                         SqlDataReader dtrSelect = cmdInsert.ExecuteReader();
-                        //display success
                         ClientScript.RegisterStartupScript(typeof(Page), "test", "<script>alert('Item successfully added to cart!');</script>");
-                        //close connection
                         con.Close();
                     }
                     else
@@ -114,21 +79,13 @@ namespace WAD_Assignment
                         Quantity = int.Parse(cmdInsert5.ExecuteScalar().ToString()) + 1;
                         con.Close();
 
-                        //create database connection
                         con = new SqlConnection(strCon);
-                        //open database
-                        con.Open();
-                        //To Get Art Id from the of the item from datalist                  
+                        con.Open();             
 
-                        //INSERT database record
                         string strInsert = "UPDATE Cart SET Quantity = " + Quantity + " WHERE CustomerId = " + Session["userID"] + "AND ArtId = " + ArtId.Text.ToString();
-                        //create sqlcommand
                         SqlCommand cmdInsert = new SqlCommand(strInsert, con);
-                        //temporary store record retrived by command object
                         SqlDataReader dtrSelect = cmdInsert.ExecuteReader();
-                        //display success
                         ClientScript.RegisterStartupScript(typeof(Page), "test", "<script>alert('Item +1 successfully added to cart!');</script>");
-                        //close connection
                         con.Close();
                     }
 
@@ -157,21 +114,13 @@ namespace WAD_Assignment
 
                     if (!itemExisted)
                     {
-                        //create database connection
                         con = new SqlConnection(strCon);
-                        //open database
                         con.Open();
-                        //To Get Art Id from the of the item from datalist
                         ArtId = e.Item.FindControl("ArtIdLabel") as Label;
-                        //INSERT database record 
                         string strInsert2 = "INSERT INTO Wishlists(CustomerId, AddedDate, ArtId) VALUES(" + Session["userID"] + "," + "GETDATE()" + "," + ArtId.Text.ToString() + ")";
-                        //create sqlcommand
                         SqlCommand cmdInsert2 = new SqlCommand(strInsert2, con);
-                        //temporary store record retrived by command object
                         SqlDataReader dtrSelect = cmdInsert2.ExecuteReader();
-                        //display success
                         ClientScript.RegisterStartupScript(typeof(Page), "test", "<script>alert('Item successfully added to wishlist!');</script>");
-                        //close connection
                         con.Close();
                         btn.ImageUrl = "https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/red_webpict35_1484337167-1.png";
                     }
--- WAD Assignment/ProductList.aspx.cs
+++ WAD Assignment/ProductList.aspx.cs
@@ -85,22 +85,14 @@ namespace WAD_Assignment
 
                     if (!itemExisted)
                     {
-                        //create database connection
                         con = new SqlConnection(strCon);
-                        //open database
                         con.Open();
-                        //To Get Art Id from the of the item from datalist
                         ArtId = e.Item.FindControl("ArtIdLabel") as Label;
                         int Quantity = 1;
-                        //INSERT database record
                         string strInsert = "INSERT INTO Cart(CustomerId, ArtId, Quantity) VALUES(" + Session["userID"] + "," + ArtId.Text.ToString() + "," + Quantity + ")";
-                        //create sqlcommand
                         SqlCommand cmdInsert = new SqlCommand(strInsert, con);
-                        //temporary store record retrived by command object
                         SqlDataReader dtrSelect = cmdInsert.ExecuteReader();
-                        //display success
                         ClientScript.RegisterStartupScript(typeof(Page), "test", "<script>alert('Item successfully added to cart!');</script>");
-                        //close connection
                         con.Close();
                     }
                     else
@@ -116,21 +108,12 @@ namespace WAD_Assignment
                         Quantity = int.Parse(cmdInsert5.ExecuteScalar().ToString()) + 1;
                         con.Close();
 
-                        //create database connection
                         con = new SqlConnection(strCon);
-                        //open database
-                        con.Open();
-                        //To Get Art Id from the of the item from datalist                  
-
-                        //INSERT database record
+                        con.Open();             
                         string strInsert = "UPDATE Cart SET Quantity = " + Quantity + " WHERE CustomerId = " + Session["userID"] + "AND ArtId = " + ArtId.Text.ToString();
-                        //create sqlcommand
                         SqlCommand cmdInsert = new SqlCommand(strInsert, con);
-                        //temporary store record retrived by command object
                         cmdInsert.ExecuteReader();
-                        //display success
                         ClientScript.RegisterStartupScript(typeof(Page), "test", "<script>alert('Item +1 successfully added to cart!');</script>");
-                        //close connection
                         con.Close();
                     }
 
@@ -159,27 +142,20 @@ namespace WAD_Assignment
 
                     if (!itemExisted)
                     {
-                        //create database connection
+                        //add to wish list
                         con = new SqlConnection(strCon);
-                        //open database
                         con.Open();
-                        //To Get Art Id from the of the item from datalist
                         ArtId = e.Item.FindControl("ArtIdLabel") as Label;
-                        //INSERT database record 
                         string strInsert2 = "INSERT INTO Wishlists(CustomerId, AddedDate, ArtId) VALUES(" + Session["userID"] + "," + "GETDATE()" + "," + ArtId.Text.ToString() + ")";
-                        //create sqlcommand
                         SqlCommand cmdInsert2 = new SqlCommand(strInsert2, con);
-                        //temporary store record retrived by command object
                         cmdInsert2.ExecuteReader();
-                        //display success
                         ClientScript.RegisterStartupScript(typeof(Page), "test", "<script>alert('Item successfully added to wishlist!');</script>");
-                        //close connection
                         con.Close();
                         btn.ImageUrl = "https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/red_webpict35_1484337167-1.png";
                     }
                     else
                     {
-
+                        //display error msg
                         btn.ImageUrl = "https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/red_webpict35_1484337167-1.png";
                         ClientScript.RegisterStartupScript(typeof(Page), "test", "<script>alert('This item is already in wishlist!');</script>");
                     }
